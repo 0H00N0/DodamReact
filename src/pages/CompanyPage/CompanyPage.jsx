3006f9b9f8e6428b218db54334471de2f806506d
hyejin021223 <gvygkfv02@naver.com>
2025-09-01 09:57:47 +0900

    board commit

---
+++ src/pages/CompanyPage/CompanyPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const CompanyPage = () => {
+
+    // === 하위 컴포넌트 정의 ===
+    const CompanyOverview = () => (
+        <div>
+            <p>도담도담은 아이들의 상상력과 즐거움을 가장 소중히 여기는 장난감 쇼핑몰입니다.</p>
+            <p>설립 : 2010년</p>
+            <p>직원 수: 50명</p>
+            <p>특징 : 안전한 장난감, 다양한 테마, 온라인/오프라인 구매 가능 </p>
+        </div>
+    );
+
+    const companyHistory = () => (
+        <ul>
+            <li>2010년 : 도담도담 창립</li>
+            <li>2013년 : 온라인 몰 오픈</li>
+            <li>2018년 : 전국 매장 5곳 확장</li>
+            <li>2023년 : AR/VR 체험존 도입</li>
+        </ul>
+    );
+
+    
+}
