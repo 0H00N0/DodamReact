4cf684a44ee86a5387c750c4f2fbb93a6ac949a2
hyejin021223 <gvygkfv02@naver.com>
2025-09-03 14:43:57 +0900

    중간 커밋

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-// src/board/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import CompanyPage from "./CompanyPage";
+import Home from "../pages/Home"; // 상대경로 주의!
+import CompanyPage from "../pages/CompanyPage/CompanyPage";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* ✅ 메뉴 영역 */}
-        <nav>
-          <ul>
-            <li><Link to="/">홈</Link></li>
-            <li><Link to="/company">회사 소개</Link></li>
+        {/* 상단 카테고리 메뉴 */}
+        <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+          <ul style={{ display: 'flex', listStyle: 'none', justifyContent: 'center', margin: 0, padding: 0 }}>
+            <li style={{ margin: '0 15px' }}><Link to="/">홈</Link></li>
+            <li style={{ margin: '0 15px' }}><Link to="/company">회사소개</Link></li>
           </ul>
         </nav>
 
-        {/* ✅ 페이지 영역 */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/company" element={<CompanyPage />} />
-        </Routes>
+        {/* 메인 내용 */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/company" element={<CompanyPage />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
--- src/board/APP.js
+++ src/board/APP.js
@@ -1,26 +1,28 @@
 // src/board/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import CompanyPage from "./CompanyPage";
+import Home from "../pages/Home";
+import CompanyPage from "../pages/CompanyPage/CompanyPage";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* ✅ 메뉴 영역 */}
+        {/* 상단 카테고리 메뉴 */}
         <nav>
           <ul>
             <li><Link to="/">홈</Link></li>
-            <li><Link to="/company">회사 소개</Link></li>
+            <li><Link to="/company">회사소개</Link></li>
           </ul>
         </nav>
 
-        {/* ✅ 페이지 영역 */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/company" element={<CompanyPage />} />
-        </Routes>
+        {/* 메인 내용 */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/company" element={<CompanyPage />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
