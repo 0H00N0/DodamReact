648e53ad8d4ba47a140ca8db8fef3be9701e0723
JungGyun7121 <ppjg1217@gmail.com>
2025-10-20 15:55:09 +0900

    10.20..인덱스 인폴트 중복

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
-import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
